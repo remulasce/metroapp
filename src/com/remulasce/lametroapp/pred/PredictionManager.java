3a7f849c666546482d6f8643f41a7b8244b36957
Fintan O'Grady <jtkirker@gmail.com>
2014-11-21 23:24:45 -0800

    PredictionManager copyonwritearraylist

--- src/com/remulasce/lametroapp/pred/PredictionManager.java
+++ src/com/remulasce/lametroapp/pred/PredictionManager.java
@@ -4,7 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -15,6 +16,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.util.Log;
+
+import com.remulasce.lametroapp.pred.PredictionManager.UpdateStager;
 
 public class PredictionManager {
 	static final String TAG = "PredictionManager";
@@ -27,7 +30,7 @@ public class PredictionManager {
 	}
 	
 	
-	protected ArrayList<Prediction> trackingList = new ArrayList<Prediction>();
+	protected List<Prediction> trackingList = new CopyOnWriteArrayList<Prediction>();
 	protected UpdateStager updater;
 	
 	public void startTracking( Prediction p ) {
