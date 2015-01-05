0c31fe7ee0bf2d4cc70bcc225560ff67c7738afc
Fintan O'Grady <jtkirker@gmail.com>
2015-01-05 01:09:14 -0800

    Skeleton autocomplete adapter, not in use

--- app/src/main/java/com/remulasce/lametroapp/components/OmniAutoCompleteAdapter.java
+++ app/src/main/java/com/remulasce/lametroapp/components/OmniAutoCompleteAdapter.java
@@ -2,15 +2,66 @@ package com.remulasce.lametroapp.components;
 
 import android.content.Context;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 
-import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by Remulasce on 1/5/2015.
  */
-public class OmniAutoCompleteAdapter extends ArrayAdapter
+public class OmniAutoCompleteAdapter extends ArrayAdapter implements Filterable
 {
+    private ArrayList<String> resultList = new ArrayList<String>();
+
     public OmniAutoCompleteAdapter(Context context, int resource) {
         super(context, resource);
+        resultList.add("Test Autocomplete");
     }
+
+
+    @Override
+    public int getCount() {
+        return resultList.size();
+    }
+
+    @Override
+    public String getItem(int index) {
+        return resultList.get(index);
+    }
+
+    @Override
+    public Filter getFilter() {
+        Filter filter = new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults filterResults = new FilterResults();
+                if (constraint != null) {
+                    // Retrieve the autocomplete results.
+                    //resultList = autocomplete(constraint.toString());
+
+                    //resultList.clear();
+
+                    // Assign the data to the FilterResults
+                    filterResults.values = resultList;
+                    filterResults.count = resultList.size();
+                }
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                if (results != null && results.count > 0) {
+                    notifyDataSetChanged();
+                }
+                else {
+                    notifyDataSetInvalidated();
+                }
+            }};
+        return filter;
+    }
+
+
+
+
 }
