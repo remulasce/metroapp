6b07d8c750e02b67b8ec00496dd993628a2775b1
Fintan O'Grady <jtkirker@gmail.com>
2015-01-13 00:00:32 -0800

    Organized initialization fxns
    Mainly just based on dependencies, so location gets started before setting
    the actionbar view, etc.

--- app/src/main/java/com/remulasce/lametroapp/MainActivity.java
+++ app/src/main/java/com/remulasce/lametroapp/MainActivity.java
@@ -61,14 +61,15 @@ public class MainActivity extends ActionBarActivity implements ServiceRequestFra
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
-        setupActionBar();
+
         startAnalytics();
-
         initializeStaticData();
-
-        linkViewReferences();
-        setupActionListeners();
         setupLocation();
+
+        setupActionBar();
+        linkViewReferences();
+
+        setupActionListeners();
         setupOmniBar();
 
         setupDefaults( getIntent() );
