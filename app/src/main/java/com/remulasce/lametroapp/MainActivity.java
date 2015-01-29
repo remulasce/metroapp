896f6d42546165c7ece4652b3095f20def4ffa6f
Fintan O'Grady <jtkirker@gmail.com>
2015-01-28 19:51:52 -0800

    rm commented out example code

--- app/src/main/java/com/remulasce/lametroapp/MainActivity.java
+++ app/src/main/java/com/remulasce/lametroapp/MainActivity.java
@@ -125,13 +125,11 @@ public class MainActivity extends ActionBarActivity implements ServiceRequestLis
             /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
-//                getActionBar().setTitle(mTitle);
             }
 
             /** Called when a drawer has settled in a completely open state. */
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-//                getActionBar().setTitle(mDrawerTitle);
             }
         };
 
