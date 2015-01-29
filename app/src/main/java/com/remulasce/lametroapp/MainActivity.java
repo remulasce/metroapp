839d4b6b96f06f2b2b921b601ea7e5a2785c8545
Fintan O'Grady <jtkirker@gmail.com>
2015-01-28 19:47:06 -0800

    Toggle about pane with icon
    But, now there's two icons.

--- app/src/main/java/com/remulasce/lametroapp/MainActivity.java
+++ app/src/main/java/com/remulasce/lametroapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.remulasce.lametroapp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -58,6 +61,8 @@ public class MainActivity extends ActionBarActivity implements ServiceRequestLis
     NetworkStatusReporter networkStatusReporter;
 
     Tracker t;
+    DrawerLayout mDrawerLayout;
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
@@ -107,6 +112,30 @@ public class MainActivity extends ActionBarActivity implements ServiceRequestLis
         mActionBar.setDisplayShowTitleEnabled(false);
         mActionBar.setCustomView(R.layout.omni_search_box);
         mActionBar.setDisplayShowCustomEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_launcher,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        ) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+//                getActionBar().setTitle(mTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+//                getActionBar().setTitle(mDrawerTitle);
+            }
+        };
+
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
     }
 
     protected void linkViewReferences() {
@@ -193,6 +222,17 @@ public class MainActivity extends ActionBarActivity implements ServiceRequestLis
                 .build() );
     }
     @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+    @Override
     protected void onStart() {
         super.onStart();
         populator.StartPopulating();
@@ -227,10 +267,16 @@ public class MainActivity extends ActionBarActivity implements ServiceRequestLis
     }
 
     @Override
-    public boolean onOptionsItemSelected( MenuItem item ) {
-        return true;
-    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle your other action bar items...
 
+        return super.onOptionsItemSelected(item);
+    }
     @Override
     public TripPopulator getTripPopulator() {
         return populator;
