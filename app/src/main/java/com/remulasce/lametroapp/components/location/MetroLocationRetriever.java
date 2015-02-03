e4be6b6273be19baf30ada88600df9c4e042621a
Fintan O'Grady <jtkirker@gmail.com>
2015-02-02 17:31:39 -0800

    Reduce location frequency
    Uses too much battery.

--- app/src/main/java/com/remulasce/lametroapp/components/location/MetroLocationRetriever.java
+++ app/src/main/java/com/remulasce/lametroapp/components/location/MetroLocationRetriever.java
@@ -87,7 +87,7 @@ public class MetroLocationRetriever implements LocationRetriever {
             }
 
             LocationRequest request = new LocationRequest();
-            request.setInterval(5 * 1000);
+            request.setInterval(60 * 1000);
             request.setFastestInterval(500);
             request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
