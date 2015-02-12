f8ef227846663e4e38f973414cf24579c2b747c9
Fintan O'Grady <jtkirker@gmail.com>
2015-02-11 18:24:55 -0800

    Returned trippopulator update time to normal after testing.

--- app/src/main/java/com/remulasce/lametroapp/TripPopulator.java
+++ app/src/main/java/com/remulasce/lametroapp/TripPopulator.java
@@ -32,7 +32,7 @@ import com.remulasce.lametroapp.libraries.SwipeDismissListViewTouchListener;
 public class TripPopulator {
     private static final String TAG = "TripPopulator";
 
-    protected final static int UPDATE_INTERVAL = 500;
+    protected final static int UPDATE_INTERVAL = 1000;
     protected Object waitLock = new Object();
     // When the swipe-to-dismiss library is working, it really doesn't want the list
     protected boolean dismissLock = false;
