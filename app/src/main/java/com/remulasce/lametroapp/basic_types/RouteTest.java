628dbca8d5ffe6fcc6ea759a491cc8ee1b0221e5
Fintan O'Grady <jtkirker@gmail.com>
2015-02-27 15:50:33 -0800

    Moved to junit 3
    Android API test helper doesn't support 4.

---
+++ app/src/main/java/com/remulasce/lametroapp/basic_types/RouteTest.java
@@ -0,0 +1,19 @@
+package com.remulasce.lametroapp.basic_types;
+
+import junit.framework.TestCase;
+
+public class RouteTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testGetColor() throws Exception {
+
+    }
+}
