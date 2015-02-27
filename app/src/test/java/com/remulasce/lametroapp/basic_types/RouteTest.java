4bbbef726a3986617959b1d4aafc0f74cf8beabd
Fintan O'Grady <jtkirker@gmail.com>
2015-02-27 15:32:46 -0800

    I made a test!

---
+++ app/src/test/java/com/remulasce/lametroapp/basic_types/RouteTest.java
@@ -0,0 +1,13 @@
+package com.remulasce.lametroapp.basic_types;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RouteTest {
+
+    @Test
+    public void testGetColor() throws Exception {
+        
+    }
+}
