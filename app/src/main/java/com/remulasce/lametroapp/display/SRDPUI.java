c3edcb736db4c12d8e74526d6318e7db822ec28f
Fintan O'Grady <jtkirker@gmail.com>
2015-03-05 01:04:39 -0800

    Removed cyclical dependency Prediction<->Trip
    Was caused by the tripupdatecallback
    Prediction shouldn't have anything to do with a Trip
    So there's now a display package with classes that will monitor
    Predictions and make Trips for them.

---
+++ app/src/main/java/com/remulasce/lametroapp/display/SRDPUI.java
@@ -0,0 +1,42 @@
+package com.remulasce.lametroapp.display;
+
+import com.remulasce.lametroapp.dynamic_data.types.Prediction;
+import com.remulasce.lametroapp.dynamic_data.types.StopRouteDestinationArrival;
+import com.remulasce.lametroapp.dynamic_data.types.StopRouteDestinationPrediction;
+import com.remulasce.lametroapp.dynamic_data.types.TripUpdateCallback;
+
+import java.util.Collection;
+
+/**
+ * Created by Remulasce on 3/5/2015.
+ *
+ * Takes a StopRouteDestinationPrediction. Makes Trips from it.
+ */
+public class SRDPUI implements PredictionUI {
+
+    StopRouteDestinationPrediction prediction;
+
+    TripUpdateCallback tripUpdateCallback;
+
+    public SRDPUI( StopRouteDestinationPrediction prediction) {
+        this.prediction = prediction;
+    }
+
+    @Override
+    public void setTripUpdateCallback(TripUpdateCallback t) {
+        tripUpdateCallback = t;
+    }
+
+    @Override
+    public void stopPredicting() {
+        prediction.stopPredicting();
+    }
+
+    @Override
+    public void predictionUpdated(Prediction p) {
+        Collection<StopRouteDestinationArrival> arrivals = prediction.getArrivals();
+        for (StopRouteDestinationArrival a : arrivals) {
+            tripUpdateCallback.tripUpdated(a.getTrip());
+        }
+    }
+}
