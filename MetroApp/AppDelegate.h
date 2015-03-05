458d9a9b5ac7d339dca27d4b2d515751aff964ce
nighelles <nighelles@berkeley.edu>
2015-03-04 17:27:58 -0800

    Initial Commit

---
+++ MetroApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MetroApp
+//
+//  Created by Nighelles on 3/4/15.
+//  Copyright (c) 2015 Nought. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
