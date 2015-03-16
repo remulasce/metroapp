5de731caf32a96e0343a42421975a81ab7101801
Fintan O'Grady <jtkirker@gmail.com>
2015-03-15 18:09:35 -0700

    Added arrival service test case file

---
+++ app/src/test/java/com/remulasce/lametroapp/ArrivalNotifyServiceTest.java
@@ -0,0 +1,27 @@
+package com.remulasce.lametroapp;
+
+import junit.framework.TestCase;
+
+public class ArrivalNotifyServiceTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testOnStartCommand() throws Exception {
+
+    }
+
+    public void testOnBind() throws Exception {
+
+    }
+
+    public void testOnDestroy() throws Exception {
+
+    }
+}
