b3d2b69cf3adcee0c51e99631d05fe4673d2e3a6
Fintan O'Grady <jtkirker@gmail.com>
2015-04-20 23:27:44 -0700

    Make empty test file

---
+++ app/src/test/java/test/java/StopServiceRequestTest.java
@@ -0,0 +1,20 @@
+package test.java;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Remulasce on 4/20/2015.
+ */
+public class StopServiceRequestTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testSanity() {}
+}
