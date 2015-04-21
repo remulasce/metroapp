a726e41e9aac383b669dbe503c50d057d1786d3a
Fintan O'Grady <jtkirker@gmail.com>
2015-04-21 00:14:24 -0700

    Test case and infrastructure for testing SR status states.
    Currently the SR doesn't actually do anything, but passes the 'nothing'
    test case.
    
    Just wrote test case for one request fetching, to implement tomorrow.
    
    I think the binary files probably shouldn't be included in git. Again the
    restructuring thing probably messed it up.
    Need to find a command to remove all files matching gitignore.

--- app/src/test/java/test/java/StopServiceRequestTest.java
+++ app/src/test/java/test/java/StopServiceRequestTest.java
@@ -1,8 +1,14 @@
 package test.java;
 
+import com.remulasce.lametroapp.java_core.basic_types.Stop;
 import com.remulasce.lametroapp.java_core.basic_types.StopServiceRequest;
+import com.remulasce.lametroapp.java_core.dynamic_data.types.Prediction;
 
 import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 /**
  * Created by Remulasce on 4/20/2015.
@@ -14,7 +20,8 @@ public class StopServiceRequestTest extends TestCase {
     public void setUp() throws Exception {
         super.setUp();
 
-        s = new StopServiceRequest();
+        Stop stop = new Stop("80301");
+        s = new StopServiceRequest(stop, "TestStopServiceRequest");
     }
 
     public void tearDown() throws Exception {
@@ -22,4 +29,82 @@ public class StopServiceRequestTest extends TestCase {
     }
 
     public void testSanity() {}
+
+    // Using the dummy s stop, make sure we're not trying to show anything
+    public void testEmptyPrediction() {
+        assertEquals("s shouldn't be displaying any status",
+                s.getTestStatusState(), StopServiceRequest.NetworkStatusState.NOTHING);
+    }
+
+    private void setPrediction(Prediction p) {
+        ArrayList<Prediction> overridePredictions = new ArrayList<Prediction>();
+        overridePredictions.add(p);
+        s.testRawSetPredictions(overridePredictions);
+    }
+
+    private void setPredictions(Prediction[] p) {
+        s.testRawSetPredictions(Arrays.asList(p));
+    }
+
+    public void testOneFetching() {
+        setPrediction(new P(Prediction.PredictionState.FETCHING));
+
+        assertEquals("Request should show status as fetching",
+                s.getTestStatusState(), StopServiceRequest.NetworkStatusState.SPINNER);
+    }
+
+
+
+    private class P extends Prediction {
+        private PredictionState state;
+        private P(PredictionState state) {
+            this.state = state;
+        }
+
+        @Override
+        public void startPredicting() {
+
+        }
+
+        @Override
+        public void stopPredicting() {
+
+        }
+
+        @Override
+        public void restoreTrips() {
+
+        }
+
+        @Override
+        public void cancelTrips() {
+
+        }
+
+        @Override
+        public boolean hasAnyPredictions() {
+            return false;
+        }
+
+        @Override
+        public int getRequestedUpdateInterval() {
+            return 0;
+        }
+
+        @Override
+        public long getTimeSinceLastUpdate() {
+            return 0;
+        }
+
+        @Override
+        public PredictionState getPredictionState() {
+            return state;
+        }
+
+        @Override
+        public String getRequestString() {
+            return null;
+        }
+    }
+
 }
