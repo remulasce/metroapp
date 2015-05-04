b0ab678be050441785d0cf811738a21a1722d0d5
Fintan O'Grady <jtkirker@gmail.com>
2015-05-04 00:30:24 -0700

    Add Agency type.
    Nothing uses it yet.

---
+++ app/src/main/java/com/remulasce/lametroapp/java_core/basic_types/Agency.java
@@ -0,0 +1,14 @@
+package com.remulasce.lametroapp.java_core.basic_types;
+
+/**
+ * Created by Remulasce on 5/4/2015.
+ *
+ * An Agency is exactly what you'd give NexTrip in the Agency field.
+ * One Agency per Stop (StopID).
+ *
+ * Note: the Los Angeles MTA has 2 agencies: lametro and lametro-rail.
+ * One is busses, the other trains.
+ */
+public class Agency {
+    public String raw = "";
+}
