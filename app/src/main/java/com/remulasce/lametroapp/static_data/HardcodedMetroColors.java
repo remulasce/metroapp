8ff9276b825582b7cc1b19533e1631ea26d1cd4a
Fintan O'Grady <jtkirker@gmail.com>
2015-05-19 20:28:44 -0700

    Use a black bar as the color of Rapid lines.
    That's how Metro wants them.
    Basically for us it distinguishes between Rapid and Local in the case in
    which either bus gets you there, so you have trips for each open.
    
    See #260

--- app/src/main/java/com/remulasce/lametroapp/static_data/HardcodedMetroColors.java
+++ app/src/main/java/com/remulasce/lametroapp/static_data/HardcodedMetroColors.java
@@ -14,14 +14,18 @@ public class HardcodedMetroColors implements RouteColorer {
 
         String raw = r.getString();
 
+        // Rail lines
         if (raw.equals("801")) { return new RouteColor("#004DAC"); }
         if (raw.equals("802")) { return new RouteColor("#EE3A43"); }
         if (raw.equals("803")) { return new RouteColor("#2EAB00"); }
         if (raw.equals("804")) { return new RouteColor("#DA7C20"); }
         if (raw.equals("805")) { return new RouteColor("#9561A9"); }
         if (raw.equals("806")) { return new RouteColor("#0177A5"); }
+        // BRT routes
         if (raw.equals("901")) { return new RouteColor("#FF5A00"); }
         if (raw.equals("910")) { return new RouteColor("#656D74"); }
+        // Rapid routes
+        if (raw.matches("^7\\d\\d$")) { return new RouteColor("#000000"); }
 
         return null;
     }
