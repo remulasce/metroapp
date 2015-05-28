497711c0071756cc415a941672553f09cf3b52d5
Fintan O'Grady <jtkirker@gmail.com>
2015-05-27 19:32:01 -0700

    Scraper takes agency names as arguments to only update those agencies.
    If you supply no arguments, scrapes all agencies, as before.
    
    Other improvements:
    - Scraper now knows how to overwrite any existing database files in the
      folder
    - Several additional print statements
    - Proposed schema for stop->routes table added, in comments.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,19 +1,33 @@
 import requests
 import xml.etree.ElementTree as ET
 import sqlite3
+import sys
 
-agencyListRequestString = "http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList"
 
-agencyListResponse = requests.get(agencyListRequestString)
+print "Specify which agencies to scrape as command-line arguments"
+print "Or leave none to scrape all agencies"
 
 agencyList = []
-root = ET.fromstring(agencyListResponse.text)
-for child in root:
-	if child.tag == "agency":
-		agencyList.append(child.attrib['tag'])
+
+
+for arg in sys.argv[1:]:
+        print arg
+        agencyList.append(arg)
+
+if len(agencyList) == 0:
+        print "No arguments given; scraping all available agencies"
+        
+        agencyListRequestString = "http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList"
+        agencyListResponse = requests.get(agencyListRequestString)
+        root = ET.fromstring(agencyListResponse.text)
+        for child in root:
+                if child.tag == "agency":
+                        agencyList.append(child.attrib['tag'])
+
+print "Scraping " + str(len(agencyList)) + " agencies"		
 		
 for agencyName in agencyList:
-	print agencyName
+	print "Now scraping: " + agencyName
 	searchString = "http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a=" + agencyName
 
 	routeListResponse = requests.get(searchString)
@@ -26,16 +40,32 @@ for agencyName in agencyList:
 
 	for child in root:
 		routeList.append(child.attrib['tag'])
-	
+
+	print "Found "+str(len(routeList))+" routes for agency "+agencyName
 	#routelist contains a list of all routes
 
-	# [STOPID, STOPNAME, LAT, LONG]
+	# [UNIQUEID, STOPID, STOPNAME, LAT, LONG]
+	stopnamesList = []
 
-	databaselist = []
-
+        # I think this is the right way to do relational databases:
+        # Each cell should basically be one datum.
+        # So, to store a mapping of stop->routes visiting that stop,
+        # you add multiple rows("records") mapping the stop to each route.
+        # So each row ROWn says "Stop s has ROUTEn"
+        #
+        # To get all the routes to a stop, you query sqlite for all the rows
+        # with stopid matching.
+        #
+        # Obviously the Internet won't tell you outright, but this makes sense to me
+        # because to add new information, you don't need to access any previous info-
+        # you just add a new row.
+        #
+        # Therefore, the stop-routes table for each agency simply has two columns:
+        # [STOPID, ROUTE]
+        stoproutesList = []
+        
 	i=0.0
 	uniquetag = 1
-	print len(routeList)
 	for route in routeList:
 		i = i + 1
 		print agencyName + " " + str(int(100*i/len(routeList))) + "%"
@@ -58,24 +88,27 @@ for agencyName in agencyList:
 							lat = child.attrib["lat"]
 							lon = child.attrib["lon"]
 							newstop = 1
-							for el in databaselist:
+							for el in stopnamesList:
 								if el[1] == stopid:
 									newstop = 0
 								#print "Match: " + str(el[0]) + ", " + str(stopid)
 							if newstop==1:
-								databaselist.append( (uniquetag,stopid,stopname,lat,lon) )
+								stopnamesList.append( (uniquetag,stopid,stopname,lat,lon) )
 								uniquetag = uniquetag + 1
-								#print databaselist
-		
+								#print stopnamesList
+
+	print "Finished scraping "+agencyName+", saving to SQL...";
 	conn = sqlite3.connect(agencyName + '.db')
 
 	c = conn.cursor()
+	# Delete old contents, if any
+	c.execute('''DROP TABLE stopnames''')
 
 	# Create table
 	c.execute('''CREATE TABLE stopnames
 	             (uniqueid integer, stopid text, stopname text, latitude real, longitude real)''')
 
-	c.executemany('INSERT INTO stopnames VALUES (?,?,?,?,?)', databaselist)
+	c.executemany('INSERT INTO stopnames VALUES (?,?,?,?,?)', stopnamesList)
 
 	# Save (commit) the changes
 	conn.commit()
@@ -83,5 +116,8 @@ for agencyName in agencyList:
 	# We can also close the connection if we are done with it.
 	# Just be sure any changes have been committed or they will be lost.
 	conn.close()
-	
+
+	print "Done with "+agencyName
+
+print "Complete."
 	
