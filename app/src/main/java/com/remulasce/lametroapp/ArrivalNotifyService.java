8e15190ad7b550227ce74d2c86ae091583d4f9d9
Fintan O'Grady <jtkirker@gmail.com>
2015-06-13 16:44:40 -0700

    Re introduce production refresh intervals on notify service.
    maximum interval has now been decreased to once every 30 seconds.

--- app/src/main/java/com/remulasce/lametroapp/ArrivalNotifyService.java
+++ app/src/main/java/com/remulasce/lametroapp/ArrivalNotifyService.java
@@ -104,8 +104,8 @@ public class ArrivalNotifyService extends Service {
 				
 				
 				try {
-					Thread.sleep(Math.min(5000 + seconds * 200, 15 * 1000));
-//					Thread.sleep(Math.min(5000 + seconds * 200, 240 * 1000)); TODO debug value
+					Thread.sleep(Math.min(5000 + seconds * 200, 30 * 1000));
+//					Thread.sleep(Math.min(5000 + seconds * 200, 240 * 1000));
 				} catch (InterruptedException e) { e.printStackTrace(); }
 			}
 		}
