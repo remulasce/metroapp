6f3d58a08d2119ff1e3184b230150570dece049c
Fintan O'Grady <jtkirker@gmail.com>
2015-07-30 17:47:02 -0700

    Settings dialog now makes a checkbox per installed agency
    I've left the hardcoded agency items in, to show how the layout looks, but
    the RegionSettingsDialogFragment deletes them all and recreates from the
    regionalizationhelper on load.
    
    Doesn't do more yet.

--- app/src/main/java/com/remulasce/lametroapp/components/regions/RegionSettingsDialogFragment.java
+++ app/src/main/java/com/remulasce/lametroapp/components/regions/RegionSettingsDialogFragment.java
@@ -5,9 +5,14 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 
 import com.remulasce.lametroapp.MainActivity;
 import com.remulasce.lametroapp.R;
+import com.remulasce.lametroapp.java_core.RegionalizationHelper;
+import com.remulasce.lametroapp.java_core.basic_types.Agency;
 
 /**
  * Created by Remulasce on 7/30/2015.
@@ -21,8 +26,27 @@ public class RegionSettingsDialogFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View view = View.inflate(getActivity(), R.layout.region_settings_pane, null);
+
+        addAgencyItems(view);
+
+
         return new AlertDialog.Builder(getActivity())
                 .setTitle(getString(R.string.settings_dialog_title))
-                .setView(View.inflate(getActivity(), R.layout.region_settings_pane, null)).create();
+                .setView(view).create();
+    }
+
+    private void addAgencyItems(View view) {
+        ViewGroup list = (ViewGroup) view.findViewById(R.id.region_settings_pane_agencies_layout);
+        list.removeAllViews();
+
+        for(Agency a : RegionalizationHelper.getInstance().getInstalledAgencies()) {
+            CheckBox agencyItem = new CheckBox(getActivity());
+            agencyItem.setText(a.raw);
+            agencyItem.setTag(a);
+
+            list.addView(agencyItem);
+        }
     }
 }
