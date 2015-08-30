40baa86ba773a34c4bf47b934f888b84e898719a
nighelles <nighelles@berkeley.edu>
2015-08-29 20:36:11 -0700

    Implemented GlobalLocationProvider on iOS
    So, it is possible to implement things java expects on the ios side, as long as you do all the interface things manually. It causes runtime errors if everything isn't perfect, but it works.
    
    So GlobalLocationProvider is implemented on ios now.
    
    Still need to add switching system with platform dep UI code.
    
    -Nighelles

---
+++ MetroApp/MetroApp/LocationUtil.h
@@ -0,0 +1,13 @@
+//
+//  LocationUtil.h
+//  MetroApp
+//
+//  Created by Nighelles on 8/29/15.
+//  Copyright (c) 2015 Nought. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationUtil : NSObject
+
+@end
---
+++ MetroApp/MetroApp/LocationUtil.m
@@ -0,0 +1,13 @@
+//
+//  LocationUtil.m
+//  MetroApp
+//
+//  Created by Nighelles on 8/29/15.
+//  Copyright (c) 2015 Nought. All rights reserved.
+//
+
+#import "LocationUtil.h"
+
+@implementation LocationUtil
+
+@end
