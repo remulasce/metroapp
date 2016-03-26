c30b76d8b855c64e489c27804caab11774c6ec2b
Fintan O'Grady <jtkirker@gmail.com>
2016-03-26 15:03:11 -0700

    Test lines works.

--- app/src/main/java/com/remulasce/lametroapp/MapActivityFragment.java
+++ app/src/main/java/com/remulasce/lametroapp/MapActivityFragment.java
@@ -2,6 +2,7 @@ package com.remulasce.lametroapp;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -20,26 +24,41 @@ public class MapActivityFragment extends Fragment implements OnMapReadyCallback 
 
     private GoogleMap mMap;
 
+    private LatLng[] testline = new LatLng[] {
+            new LatLng(37.423742, -122.089998),
+            new LatLng(37.421009, -122.086690),
+            new LatLng(37.420525, -122.077949),
+            new LatLng(37.406214, -122.078102),
+    };
+
     public MapActivityFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager()
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+
+        FragmentManager fragmentManager = getChildFragmentManager();
+        SupportMapFragment mapFragment = (SupportMapFragment) fragmentManager
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        return inflater.inflate(R.layout.fragment_map, container, false);
+        return view;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng google = new LatLng(37.4164323,-122.0761531);
+        mMap.addMarker(new MarkerOptions().position(google).title("Marker on Google"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(google));
+
+        PolylineOptions options = new PolylineOptions();
+        for ( int i = 0; i < testline.length; i++) {
+            options.add(testline[i]);
+        }
+        Polyline line = googleMap.addPolyline(options);
     }
 }
