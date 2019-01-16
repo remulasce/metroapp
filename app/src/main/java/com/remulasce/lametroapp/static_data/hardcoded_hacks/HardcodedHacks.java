d7df05e0e0e06ab7c54c169f32fd35ec1899d5c0
Fintan O'Grady <jtkirker@gmail.com>
2019-01-15 22:20:55 -0800

    Update stop files.

--- app/src/main/java/com/remulasce/lametroapp/static_data/hardcoded_hacks/HardcodedHacks.java
+++ app/src/main/java/com/remulasce/lametroapp/static_data/hardcoded_hacks/HardcodedHacks.java
@@ -9,7 +9,7 @@ import com.remulasce.lametroapp.java_core.basic_types.Agency;
 
 public class HardcodedHacks {
 
-    public static final int DATABASE_VERSION = 25;
+    public static final int DATABASE_VERSION = 26;
 
     public static boolean useBay511(Agency agency) {
         return agencyMatchesAny(agency, "caltrain", "SamTrans", "VTA");
