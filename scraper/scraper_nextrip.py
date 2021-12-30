eeed934307e347beca8214636eca4370a018ef0b
Faelyn O'Grady <jtkirker@gmail.com>
2021-12-29 20:04:00 -0800

    Update scraper, part 1
    Builds an agency name dict for all inputs, including command line
    Scrape minimum supported agencies by default to avoid rate limiting
    Reformat python file a bit to look less like a a hack.

--- scraper/scraper_nextrip.py
+++ scraper/scraper_nextrip.py
@@ -6,187 +6,205 @@ import sys
 OUTPUT_DIR = "_out/"
 
 print("Specify which agencies to scrape as command-line arguments")
-print("Or leave none to scrape all agencies")
+print("Or leave none to scrape only supported agencies")
 print("Command-line agencies won't have their proper titles scraped")
 
-agencyList = []
+agencies_to_scrape = []
 
 for arg in sys.argv[1:]:
     print(arg)
-    agencyList.append((arg, arg))
+    agencies_to_scrape.append((arg, arg))
 
-if len(agencyList) == 0:
-    print("No arguments given; scraping all available agencies")
+if len(agencies_to_scrape) == 0:
+    print("No arguments given; scraping sane defaults")
 
-    agencyListRequestString = "http://retro.umoiq.com/service/publicXMLFeed?command=agencyList"
-    agencyListResponse = requests.get(agencyListRequestString)
-    root = ET.fromstring(agencyListResponse.text)
-    for child in root:
-        if child.tag == "agency":
-            agencyList.append((child.attrib['tag'], child.attrib['title']))
+    agencies_to_scrape = ["lametro", "lametro-rail", "sf-muni"]
 
-print("Scraping " + str(len(agencyList)) + " agencies")
 
-for agencyParam in agencyList:
-    agencyName = agencyParam[0]
-    print("Now scraping: " + agencyName)
-    searchString = "http://retro.umoiq.com/service/publicXMLFeed?command=routeList&a=" + agencyName
+# This likely trips their ratelimiting
+def get_all_agencies_dict():
+    all_agencies = dict()  # raw, displayname
+    agency_list_request_string = "http://retro.umoiq.com/service/publicXMLFeed?command=agencyList"
+    agency_list_response = requests.get(agency_list_request_string)
+    tree_root = ET.fromstring(agency_list_response.text)
+    for tree_child in tree_root:
+        if tree_child.tag == "agency":
+            tag = tree_child.attrib['tag']
+            display_name = tree_child.attrib['title']
+            all_agencies[tag] = display_name
+    if not len(all_agencies):
+        raise Error("Couldn't create agencies mapping")
+    return all_agencies
 
-    routeListResponse = requests.get(searchString)
 
-    root = ET.fromstring(routeListResponse.text)
+print("Scraping " + str(len(agencies_to_scrape)) + " agencies")
 
-    routeList = []
 
-    # get all the routes so we can scrape the database
+def main():
+    agency_names_dict = get_all_agencies_dict()
+    
+    for agency_tag in agencies_to_scrape:
+        agency_display_name = agency_names_dict[agency_tag]
+        print("Now scraping: " + agency_tag + ", " + agency_display_name)
+        search_string = "http://retro.umoiq.com/service/publicXMLFeed?command=routeList&a=" + agency_tag
 
-    for child in root:
-        routeList.append(child.attrib['tag'])
+        route_list_response = requests.get(search_string)
 
-    print("Found " + str(len(routeList)) + " routes for agency " + agencyName)
-    # routelist contains a list of all routes
+        root = ET.fromstring(route_list_response.text)
 
-    agencyid = agencyName
-    agencytitle = agencyParam[1]
-    agencyLatMin = None;
-    agencyLatMax = None;
-    agencyLonMin = None;
-    agencyLonMax = None;
+        routeList = []
 
-    # [AGENCYID, AGENCYTITLE, LATMIN, LATMAX, LONMIN, LONMAX]
-    agencyInfo = []
+        # get all the routes so we can scrape the database
 
-    # [UNIQUEID, STOPID, STOPNAME, LAT, LONG]
-    stopnamesList = []
+        for child in root:
+            routeList.append(child.attrib['tag'])
 
-    # I think this is the right way to do relational databases:
-    # Each cell should basically be one datum.
-    # So, to store a mapping of stop->routes visiting that stop,
-    # you add multiple rows("records") mapping the stop to each route.
-    # So each row ROWn says "Stop s has ROUTEn"
-    #
-    # To get all the routes to a stop, you query sqlite for all the rows
-    # with stopid matching.
-    #
-    # Obviously the Internet won't tell you outright, but this makes sense to me
-    # because to add new information, you don't need to access any previous info-
-    # you just add a new row.
-    #
-    # Therefore, the stop-routes table for each agency simply has two columns:
-    # [STOPID, ROUTE]
-    stoproutesList = []
+        print("Found " + str(len(routeList)) + " routes for agency " + agency_tag)
+        # routelist contains a list of all routes
 
-    # For future conversion from stopid to route + stopTag
-    # This will avoid some issues with agencies not assigning stopids to some stops
-    # Technically they're optional. It's just some agencies fuck up, like, 2 of them.
-    #
-    # Note that this isn't just an alternate way of calling stopid. Some stopid stops
-    # will be split into multiple route + stopTags.
-    # [STOPNAME, ROUTE, STOPTAG]
-    stopnameroutetagList = []
+        agencyid = agency_tag
+        agencyLatMin = None;
+        agencyLatMax = None;
+        agencyLonMin = None;
+        agencyLonMax = None;
 
-    i = 0.0
-    uniquetag = 1
-    for routeTag in routeList:
-        i = i + 1
-        print(agencyName + " " + str(int(100 * i / len(routeList))) + "%")
-        routeRequestString = "http://retro.umoiq.com/service/publicXMLFeed?command=routeConfig&a=" + agencyName + "&r=" + routeTag
-        routeResponse = requests.get(routeRequestString)
+        # [AGENCYID, AGENCYTITLE, LATMIN, LATMAX, LONMIN, LONMAX]
+        agencyInfo = []
 
-        print(routeRequestString)
+        # [UNIQUEID, STOPID, STOPNAME, LAT, LONG]
+        stopnamesList = []
 
-        root = ET.fromstring(routeResponse.text)
-        for routeObject in root:
-            if routeObject.tag == "route":
-                # Collect the bounds of each route to calculate the agency's total service area
-                # Add .5 lon / lat to show where we could possibly want data. This is pretty far away from the agency.
+        # I think this is the right way to do relational databases:
+        # Each cell should basically be one datum.
+        # So, to store a mapping of stop->routes visiting that stop,
+        # you add multiple rows("records") mapping the stop to each route.
+        # So each row ROWn says "Stop s has ROUTEn"
+        #
+        # To get all the routes to a stop, you query sqlite for all the rows
+        # with stopid matching.
+        #
+        # Obviously the Internet won't tell you outright, but this makes sense to me
+        # because to add new information, you don't need to access any previous info-
+        # you just add a new row.
+        #
+        # Therefore, the stop-routes table for each agency simply has two columns:
+        # [STOPID, ROUTE]
+        stoproutesList = []
 
-                routeLatMin = float(routeObject.attrib["latMin"]) - .5;
-                routeLonMin = float(routeObject.attrib["lonMin"]) - .5;
-                routeLatMax = float(routeObject.attrib["latMax"]) + .5;
-                routeLonMax = float(routeObject.attrib["lonMax"]) + .5;
-                # print "Found area bounds for route "+routeObject.attrib["title"]+": "+routeLatMin+" "+routeLatMax+" "+routeLonMin+" "+routeLonMax
+        # For future conversion from stopid to route + stopTag
+        # This will avoid some issues with agencies not assigning stopids to some stops
+        # Technically they're optional. It's just some agencies fuck up, like, 2 of them.
+        #
+        # Note that this isn't just an alternate way of calling stopid. Some stopid stops
+        # will be split into multiple route + stopTags.
+        # [STOPNAME, ROUTE, STOPTAG]
+        stopnameroutetagList = []
 
-                if (agencyLatMin == None):
-                    agencyLatMin = routeLatMin
-                    agencyLatMax = routeLatMax
-                    agencyLonMin = routeLonMin
-                    agencyLonMax = routeLonMax
-                else:
-                    agencyLatMin = min(routeLatMin, agencyLatMin)
-                    agencyLatMax = max(routeLatMax, agencyLatMax)
-                    agencyLonMin = min(routeLonMin, agencyLonMin)
-                    agencyLonMax = max(routeLonMax, agencyLonMax)
+        i = 0.0
+        uniquetag = 1
+        for routeTag in routeList:
+            i = i + 1
+            print(agency_tag + " " + str(int(100 * i / len(routeList))) + "%")
+            routeRequestString = "http://retro.umoiq.com/service/publicXMLFeed?command=routeConfig&a=" + agency_tag + "&r=" + routeTag
+            routeResponse = requests.get(routeRequestString)
 
-                for child in routeObject:
-                    if child.tag == "stop":
-                        # Don't know if stoptag needs some cleaning like stopid
-                        stopname = child.attrib["title"]
-                        stoptag = child.attrib["tag"]
-                        stopnameroutetagTuple = (stopname, routeTag, stoptag)
-                        if not stopnameroutetagTuple in stopnameroutetagList:
-                            stopnameroutetagList.append(stopnameroutetagTuple)
-                        # print stopnameroutetagTuple
+            print(routeRequestString)
 
-                        if "stopId" in list(child.attrib.keys()):
-                            # print child.attrib
-                            # uniquetag = child.attrib["tag"]
-                            stopid = child.attrib["stopId"]
-                            stopid = stopid.lstrip("0");
+            root = ET.fromstring(routeResponse.text)
+            for routeObject in root:
+                if routeObject.tag == "route":
+                    # Collect the bounds of each route to calculate the agency's total service area
+                    # Add .5 lon / lat to show where we could possibly want data. This is pretty far away from the agency.
 
-                            lat = child.attrib["lat"]
-                            lon = child.attrib["lon"]
-                            newstop = 1
-                            for el in stopnamesList:
-                                if el[1] == stopid:
-                                    newstop = 0
-                            # print "Match: " + str(el[0]) + ", " + str(stopid)
-                            if newstop == 1:
-                                stopnamesList.append((uniquetag, stopid, stopname, lat, lon))
-                                uniquetag = uniquetag + 1
-                            # print stopnamesList
+                    routeLatMin = float(routeObject.attrib["latMin"]) - .5;
+                    routeLonMin = float(routeObject.attrib["lonMin"]) - .5;
+                    routeLatMax = float(routeObject.attrib["latMax"]) + .5;
+                    routeLonMax = float(routeObject.attrib["lonMax"]) + .5;
+                    # print "Found area bounds for route "+routeObject.attrib["title"]+": "+routeLatMin+" "+routeLatMax+" "+routeLonMin+" "+routeLonMax
 
-                            stoprouteTuple = (stopid, routeTag)
-                            if not stoprouteTuple in stoproutesList:
-                                stoproutesList.append(stoprouteTuple)
-                            # print stoproutesList
+                    if (agencyLatMin == None):
+                        agencyLatMin = routeLatMin
+                        agencyLatMax = routeLatMax
+                        agencyLonMin = routeLonMin
+                        agencyLonMax = routeLonMax
+                    else:
+                        agencyLatMin = min(routeLatMin, agencyLatMin)
+                        agencyLatMax = max(routeLatMax, agencyLatMax)
+                        agencyLonMin = min(routeLonMin, agencyLonMin)
+                        agencyLonMax = max(routeLonMax, agencyLonMax)
 
-    print("Finished scraping " + agencyName + ", saving to SQL...");
-    conn = sqlite3.connect(OUTPUT_DIR + agencyName + '.db')
+                    for child in routeObject:
+                        if child.tag == "stop":
+                            # Don't know if stoptag needs some cleaning like stopid
+                            stopname = child.attrib["title"]
+                            stoptag = child.attrib["tag"]
+                            stopnameroutetagTuple = (stopname, routeTag, stoptag)
+                            if not stopnameroutetagTuple in stopnameroutetagList:
+                                stopnameroutetagList.append(stopnameroutetagTuple)
+                            # print stopnameroutetagTuple
 
-    c = conn.cursor()
-    # Delete old contents, if any
-    c.execute('''DROP TABLE IF EXISTS stopnames''')
-    c.execute('''DROP TABLE IF EXISTS stoproutes''')
-    c.execute('''DROP TABLE IF EXISTS stopnameroutetags''')
-    c.execute('''DROP TABLE IF EXISTS agencyinfo''')
+                            if "stopId" in list(child.attrib.keys()):
+                                # print child.attrib
+                                # uniquetag = child.attrib["tag"]
+                                stopid = child.attrib["stopId"]
+                                stopid = stopid.lstrip("0");
 
-    # Create table
-    c.execute('''CREATE TABLE stopnames
-	             (uniqueid integer, stopid text, stopname text, latitude real, longitude real)''')
-    # Correct way to make primary keys, Nighelles
-    c.execute('''CREATE TABLE stoproutes
-	             ( stopid text, route text)''')
-    # ...Is to ignore them. Sqlite will add one for you anyway, and increment it itself.
-    c.execute('''CREATE TABLE stopnameroutetags
-	             ( stopname text, route text, stoptag text)''')
-    # Overall info about the agency. Agencyid should be internal id, like lametro-rail, with the name being user-facing
-    c.execute('''CREATE TABLE agencyinfo
-                     ( agencyid text, agencyname text, latMin real, latMax real, lonMin real, lonMax real )''')
+                                lat = child.attrib["lat"]
+                                lon = child.attrib["lon"]
+                                newstop = 1
+                                for el in stopnamesList:
+                                    if el[1] == stopid:
+                                        newstop = 0
+                                # print "Match: " + str(el[0]) + ", " + str(stopid)
+                                if newstop == 1:
+                                    stopnamesList.append((uniquetag, stopid, stopname, lat, lon))
+                                    uniquetag = uniquetag + 1
+                                # print stopnamesList
 
-    c.executemany('INSERT INTO stopnames VALUES (?,?,?,?,?)', stopnamesList)
-    c.executemany('INSERT INTO stoproutes VALUES (?,?)', stoproutesList)
-    c.executemany('INSERT INTO stopnameroutetags VALUES (?,?,?)', stopnameroutetagList)
-    c.execute('INSERT INTO agencyinfo VALUES (?,?,?,?,?,?)',
-              (agencyid, agencytitle, agencyLatMin, agencyLatMax, agencyLonMin, agencyLonMax))
+                                stoprouteTuple = (stopid, routeTag)
+                                if not stoprouteTuple in stoproutesList:
+                                    stoproutesList.append(stoprouteTuple)
+                                # print stoproutesList
 
-    # Save (commit) the changes
-    conn.commit()
+        print("Finished scraping " + agency_tag + ", saving to SQL...");
+        conn = sqlite3.connect(OUTPUT_DIR + agency_tag + '.db')
 
-    # We can also close the connection if we are done with it.
-    # Just be sure any changes have been committed or they will be lost.
-    conn.close()
+        c = conn.cursor()
+        # Delete old contents, if any
+        c.execute('''DROP TABLE IF EXISTS stopnames''')
+        c.execute('''DROP TABLE IF EXISTS stoproutes''')
+        c.execute('''DROP TABLE IF EXISTS stopnameroutetags''')
+        c.execute('''DROP TABLE IF EXISTS agencyinfo''')
 
-    print("Done with " + agencyName)
+        # Create table
+        c.execute('''CREATE TABLE stopnames
+                     (uniqueid integer, stopid text, stopname text, latitude real, longitude real)''')
+        # Correct way to make primary keys, Nighelles
+        c.execute('''CREATE TABLE stoproutes
+                     ( stopid text, route text)''')
+        # ...Is to ignore them. Sqlite will add one for you anyway, and increment it itself.
+        c.execute('''CREATE TABLE stopnameroutetags
+                     ( stopname text, route text, stoptag text)''')
+        # Overall info about the agency. Agencyid should be internal id, like lametro-rail, with the name being user-facing
+        c.execute('''CREATE TABLE agencyinfo
+                         ( agencyid text, agencyname text, latMin real, latMax real, lonMin real, lonMax real )''')
+
+        c.executemany('INSERT INTO stopnames VALUES (?,?,?,?,?)', stopnamesList)
+        c.executemany('INSERT INTO stoproutes VALUES (?,?)', stoproutesList)
+        c.executemany('INSERT INTO stopnameroutetags VALUES (?,?,?)', stopnameroutetagList)
+        c.execute('INSERT INTO agencyinfo VALUES (?,?,?,?,?,?)',
+                  (agencyid, agency_display_name, agencyLatMin, agencyLatMax, agencyLonMin, agencyLonMax))
+
+        # Save (commit) the changes
+        conn.commit()
+
+        # We can also close the connection if we are done with it.
+        # Just be sure any changes have been committed or they will be lost.
+        conn.close()
+
+        print("Done with " + agency_tag)
+
+
+main()
 
 print("Complete.")
